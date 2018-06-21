2272ed303e2b413622884b9a6e281314dcaca7f1
伊藤和樹 <bump.of.chicken.fan@gmail.com>
2018-06-21 20:47:07 +0900

    [Add]AssetBundleManagerConfig

---
+++ Assets/Scripts/AssetBundle/AssetBundleManagerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class AssetBundleManagerConfig
+{
+    /// <summary>
+    /// AssetBundle Dirivery Server URL
+    /// </summary>
+    public readonly static string ServerURL = "http://ServerURL";
+
+    public readonly static string AssetBundleInfoListFileName = "AssetBundleInfoList.json";
+
+}
